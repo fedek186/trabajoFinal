35d1fd83d0e6031bbf1960303f8d260439f065f6
Foncho81z <alfonsoagote@gmail.com>
2022-06-08 21:44:57 -0300

    corrigiendo una fk

--- base_de_datos_query.sql
+++ base_de_datos_query.sql
@@ -11,32 +11,35 @@ CREATE TABLE `usuarios` (
   PRIMARY KEY (`id`)
 );
 CREATE TABLE `productos` (
-  `id_producto` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL,
   `foto` varchar(1000) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `descripcion` varchar(2000) NOT NULL,
   `fecha` date NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `Id_usuario` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id_producto`),
-  KEY `idUsuarioProductos` (`Id_usuario`),
-  CONSTRAINT `idUsuarioProductos` FOREIGN KEY (`Id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `id_usuario` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idUsuarioProductos` (`id_usuario`),
+  CONSTRAINT `idUsuarioProductos` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 CREATE TABLE `comentarios` (
-  `id_comentarios` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `comentario` varchar(3000) NOT NULL,
   `id_usuario` int(10) unsigned NOT NULL,
   `fecha` date NOT NULL,
-  PRIMARY KEY (`id_comentarios`),
-  KEY `idUsuario` (`id_usuario`),
-  CONSTRAINT `idUsuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `id_producto` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idUsuarioComentarios` (`id_usuario`),
+  KEY `idProductoComentarios` (`id_producto`),
+  CONSTRAINT `idProductoComentarios` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `idUsuarioComentarios` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ); 
 CREATE TABLE `seguidor_seguidos` (
   `id_seguidor` int(10) unsigned NOT NULL,
   `id_seguido` int(10) unsigned NOT NULL,
-  `id_relacion` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id_relacion`),
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
   KEY `idSeguido` (`id_seguido`),
   KEY `idSeguidor` (`id_seguidor`),
   CONSTRAINT `idSeguido` FOREIGN KEY (`id_seguido`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
