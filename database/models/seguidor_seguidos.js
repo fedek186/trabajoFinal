99425da20645de5a9f9621659b8405606554efc3
Foncho81z <alfonsoagote@gmail.com>
2022-06-10 09:46:17 -0300

    agregué un modelo

---
+++ database/models/seguidor_seguidos.js
@@ -0,0 +1,30 @@
+module.exports = function (sequelize, dataTypes) {
+    
+    /* Alias que le asigne */
+    let alias = "Seguidor";
+
+    /* Configuraciones de las columnas de la tabla */
+    let cols = {
+        id:{
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER,
+        },
+        id_seguidor: {
+            type: dataTypes.INTEGER,
+        },
+        id_seguido: {
+            type: dataTypes.INTEGER,
+        }
+    }
+
+    let config = {
+        tableName: 'Seguidor', 
+        timestamps: true, //Si la tabla no tiene los campos created_at y updated_at
+        underscored: true, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.
+    }
+
+    const Seguidor = sequelize.define(alias, cols, config);
+
+    return Seguidor;
+}
