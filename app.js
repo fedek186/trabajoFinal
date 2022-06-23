1194b78db5a532e4fb0a20f635d0cefefb8a7a35
krikodium <mkrikorian@udesa.edu.ar>
2022-06-23 11:34:56 -0300

    jjajja

--- app.js
+++ app.js
@@ -23,56 +23,57 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'))); //Todos los archivos que esten dentro de la carpeta public son estaticos.
 
 //* Configuramos el session 
-app.use(session( { 
-  secret: "NWZapatillas", 
-  resave: false, 
+app.use(session({
+  secret: "NWZapatillas",
+  resave: false,
   saveUninitialized: true
 }));
 
 //* Middleware de session. Debe estar antes de la declaración de las rutas. 
-app.use(function(req,res, next) {
-   //* Aca evaluo la condicion creada en el procesarLogin
-  if(req.session.user != undefined) {
+app.use(function (req, res, next) {
+  //* Aca evaluo la condicion creada en el procesarLogin
+  if (req.session.user != undefined) {
     res.locals.user = req.session.user
-    return next ()
+    return next()
   }
-  return next () //* Si no entra en el if que siga al próximo middleware
+  return next() //* Si no entra en el if que siga al próximo middleware
 });
 
 //* Middleware de session. Debe estar antes de la declaración de las rutas. 
-app.use(function(req,res, next) {
+app.use(function (req, res, next) {
   //* Aca evaluo la condicion creada en el procesarLogin
- if(req.session.errors != undefined) {
-   res.locals.errors = req.session.errors;
-   return next ()
- }
- return next () //* Si no entra en el if que siga al próximo middleware
+  if (req.session.errors != undefined) {
+    res.locals.errors = req.session.errors;
+    return next()
+  }
+  return next() //* Si no entra en el if que siga al próximo middleware
 });
 
 //* Middleware de cookies. 
-app.use(function (req,res,next) {
+app.use(function (req, res, next) {
   if (req.cookies.userId != undefined && req.session.user == undefined) {
     //* la cookie se "activa" cuando se pierde la sesion
     let idUsuarioEnCookie = req.cookies.userId;
 
     db.Usuario.findByPk(idUsuarioEnCookie)
-    .then((result) => {
-      req.session.user = result.dataValues;
-      res.locals.user = req.session.user; 
-      return next();
-    }).catch (function (errores) {
-      console.log(errores);
-    });
-  } 
-    else {
-      return next ();
+      .then((result) => {
+        req.session.user = result.dataValues;
+        res.locals.user = req.session.user;
+        return next();
+      }).catch(function (errores) {
+        console.log(errores);
+      });
+  } else {
+    return next();
   }
-}) 
+})
 
 app.use('/', indexRouter);
 app.use('/product', productRouter);
@@ -80,12 +81,12 @@ app.use('/user', userRouter);
 app.use('/search', searchRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
